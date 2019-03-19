2f8ba67a2cae54937a22f9ff99283bd3a9921b04
Tim Bowes <timber.trader@Tims-MacBook-Pro.local>
2019-03-19 15:13:58 -0600

    added store count ... missing in first commit

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -9,3 +9,4 @@ puts "----------"
  newStore = Store.create(name: 'Richmond', annual_revenue: 126000, mens_apparel: false, womens_apparel: true)
  newStore = Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
  newStore.save
+puts Store.count
