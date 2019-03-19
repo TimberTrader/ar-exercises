e6dd5543657b2ee2759ad7b62f5c0e0a17d3a65f
Tim Bowes <timber.trader@Tims-MacBook-Pro.local>
2019-03-19 16:55:52 -0600

    new stores added to db

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,18 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+newStore = Store.create(name: 'Surrey', annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+newStore = Store.create(name: 'Whistler', annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+newStore = Store.create(name: 'Yaletown', annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+newStore.save
+puts Store.count
+# @Store3 = Store.find_by(name: 'Surrey')
+# @Store4 = Store.find_by(name: 'Whistler')
+# @Store5 = Store.find_by(name: 'Yaletown')
+
+
+
+
+
+
